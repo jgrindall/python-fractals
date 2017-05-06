63ece839d49dbf4a29a6b31c9cb78ba6d2a03cca
john.grindall@gmail.com <john.grindall@gmail.com>
2017-05-06 17:47:23 +0100

    cantor set

--- app.py
+++ app.py
@@ -1,28 +1,9 @@
-print ("hi")
+from tkinter import *
 
 myList =[1,3,5]
-
-print(myList[1]);
-
 myList.append(7);
-
-print(myList[1]);
-
 myList.remove(3);
-
-print(myList[1]);
-
 myTuple = (2, 4, 6);
-
-print(myTuple[0]);
-
-myList2 = [(0,1), (2,0)]
-
-print(myList2)
-
-for i in (1,2,3):
-    print(i)
-
 for i in range(1,6):
     print(i)
 
@@ -36,3 +17,25 @@ def fibo(n):
     print(x)
 
 fibo(9);
+
+
+root=Tk()
+win = Canvas(root,width=1000, height=600)
+win.pack()
+def stopProg(e):
+    root.destroy()
+
+win.master.title("Testing...")
+
+def cantor_set(x, y, len):
+    if(len > 0.5):
+        win.create_line(x, y, x + len, y)
+        y = y + 40
+        cantor_set(x, y, len/3)
+        cantor_set(x + (2/3)*len, y, len/3)
+
+cantor_set(50,50,800);
+
+
+
+root.mainloop()
